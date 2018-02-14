086abc550f2178ff3bd09b9330635fa0e9e9c678
girisagar46 <girisagar46@gmail.com>
2018-02-14 22:07:28 +0545

    Django Query set

--- Week_2/DjangoBlog/blog/views.py
+++ Week_2/DjangoBlog/blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
 
 def home(request):
-    return render(request, 'home.html', context={})
+    object_list = Post.objects.order_by("-created_date")
+    count = object_list.count()
+    ctx = {
+        "object_list": object_list,
+        "count": count
+    }
+    return render(request, 'home.html', context=ctx)
 
 def about(request):
     return render(request, 'about.html', context={})
